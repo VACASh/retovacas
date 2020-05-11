64737183c8f15591ed7c2a1bb6de68cafccd1aac
nacho vacas matencio <chvacas@hotmail.com>
2020-05-11 22:30:12 +0200

    Updates

---
+++ precache-manifest.e2c0e614a1ea279a23f8daa1d6373707.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afcdf22aa336a965a342845b17293ac6",
+    "url": "/retovacas/index.html"
+  },
+  {
+    "revision": "11f6a2cf355605a231c8",
+    "url": "/retovacas/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f7b71ebd8727ce818db",
+    "url": "/retovacas/static/js/2.fcc2a5d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/retovacas/static/js/2.fcc2a5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f6a2cf355605a231c8",
+    "url": "/retovacas/static/js/main.88159be6.chunk.js"
+  },
+  {
+    "revision": "3066f212a4eadc773665",
+    "url": "/retovacas/static/js/runtime-main.8cc517a1.js"
+  },
+  {
+    "revision": "d9a0e9614b760fb4ef36c24f8a790179",
+    "url": "/retovacas/static/media/esther.d9a0e961.mp4"
+  },
+  {
+    "revision": "96f24f59d0f2b8f72f401927753b77e4",
+    "url": "/retovacas/static/media/mariona.96f24f59.mp4"
+  },
+  {
+    "revision": "0a8130d99d19e128af2c16dcc6c0db53",
+    "url": "/retovacas/static/media/vacas.0a8130d9.jpg"
+  }
+]);
